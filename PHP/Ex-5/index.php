d22690db1a2f5fd44de7d43e5093a2cb05c8563e
DelannoyAxel <delannoy.axel2@gmail.com>
2024-05-27 16:28:42 +0200

    push php

--- PHP/Ex-5/index.php
+++ PHP/Ex-5/index.php
@@ -4,7 +4,7 @@
 // 1°) Écrire un programme qui déclare un tableau de 9 notes, dont on fait ensuite saisir les valeurs par
 // utilisateurs.
 
-// $tab =[];
+// $tab = [];
 
 // for ($i=0; $i < 9 ; $i++) { 
 // $saisie = readline("entrez un nombre: ");
@@ -20,29 +20,125 @@
 // valeurs qu’il compte saisir. Il effectuera ensuite cette saisie. Enfin, une fois la saisie terminée, le
 // programme affichera le nombre de valeurs négatives et le nombre de valeurs positives.
 
-$tab = [];
+// $tab = [];
 
-$saisie1 = readline('écrit le nombre de valeur que tu va entrer: ');
+// $saisie1 = readline('écrit le nombre de valeur que tu va entrer: ');
 
-for ($i=0; $i < $saisie1; $i++) { 
-    $saisie2 = readline('ecrit une valeur a ajouter au tableau: ');
-    $tab[] = $saisie2;
-}
+// for ($i=0; $i < $saisie1; $i++) { 
+//     $saisie2 = readline('ecrit une valeur a ajouter au tableau: ');
+//     $tab[] = $saisie2;
+// }
 
-$positives = 0;
-$negatives = 0;
+// $positives = 0;
+// $negatives = 0;
 
-foreach ($tab as $value) {
+// foreach ($tab as $value) {
 
-    if ($value < 0){
-        $negatives++;
-    }elseif ($value > 0)
-    $positives++;
-}
+//     if ($value < 0){
+//         $negatives++;
+//     }elseif ($value > 0)
+//     $positives++;
+// }
 
-echo "nombre de valeur negative = " , $negatives , "\n";
-echo "nombre de valeur positive = " , $positives , "\n";
-
-
+// echo "nombre de valeur negative = " , $negatives , "\n";
+// echo "nombre de valeur positive = " , $positives , "\n";
 
 
+// 3°) Ecrivez un algorithme calculant la somme des valeurs d’un tableau (on suppose que le tableau a
+// été préalablement saisi).
+
+
+// $tab = [1, 2, 3, 4, 5];
+// $sum = 0;
+
+// for ($i = 0; $i < count($tab); $i++) {
+//     $sum += $tab[$i];
+// }
+
+// echo "La somme des valeurs du tableau est : " . $sum;
+
+// 4°) Ecrivez un algorithme constituant un tableau, à partir de deux tableaux de même longueur
+// préalablement saisis. Le nouveau tableau sera la somme des éléments des deux tableaux de départ.
+
+// $tab1 = [4,8,7,9,1,5,4,6];
+// $tab2 = [7,6,5,2,1,3,7,4];
+// $tab3 = [];
+
+
+// for ($i=0; $i < count($tab1); $i++) {
+// $resultat = $tab1[$i] + $tab2[$i];
+// array_push($tab3, $resultat);
+
+// }
+
+// echo "La somme des valeurs des deux tableaux est : " . implode(", ", $tab3);
+
+// 5°) Ecrivez un algorithme permettant, toujours sur le même principe, à l’utilisateur de saisir un
+// nombre déterminé de valeurs. Le programme, une fois la saisie terminée, renvoie la plus grande
+// valeur en précisant quelle position elle occupe dans le tableau. On prendra soin d’effectuer la saisie
+// dans un premier temps, et la recherche de la plus grande valeur du tableau dans un second temps.
+
+// $tab = [];
+
+// for ($i=0; $i < 5 ; $i++) { 
+//     $saisie = readline("entrez un nombre : ");
+//     array_push($tab, $saisie);
+
+// };
+
+// $maxValue = $tab[0];
+// $position = 0;
+
+// for ($j = 1; $j < count($tab); $j++) { 
+//     if ($maxValue < $tab[$j]) { 
+
+//         $maxValue = $tab[$j];
+//         $position = $j;
+//     }
+// }
+
+// echo "La plus grande valeur du tableau est : " . $maxValue . "\n";
+// echo "Elle se trouve à la position : " . $position . "\n";
+
+// 6°) Toujours et encore sur le même principe, écrivez un algorithme permettant, à l’utilisateur de saisir
+// les notes d'une classe. Le programme, une fois la saisie terminée, renvoie le nombre de ces notes
+// supérieures à la moyenne de la classe.
+
+
+// $tab = [];
+
+
+// for ($i = 0; $i < 12; $i++) { 
+//     $note = readline("Indiquez la note de l'élève " , ($i+1) , " : ");
+//     array_push($tab, $note); 
+// }
+
+// $sum = array_sum($tab);
+// $moyenne = $sum / count($tab);
+
+// echo "La moyenne de la classe est : " , $moyenne . "\n";
+
+// echo "Les notes supérieures à la moyenne sont : ";
+// foreach ($tab as $note) {
+//     if ($note > $moyenne) {
+//         echo $note , " ";
+//     }
+// }
+
+// 7°) A partir de deux tableaux contenant l’un des prix et l’autre des quantités de produits achetés,
+// écrire un programme permettant de calculer le prix total.
+
+// $prix = [5, 50, 23, 11];
+// $quantite = [10, 1, 4, 3];  
+// $tab3 = [];
+
+// for ($i = 0; $i < count($prix); $i++) {
+//     $tab3[] = $prix[$i] * $quantite[$i]; 
+// }
+
+// foreach ($tab3 as $value) {
+//     $value;
+// }
+
+// $total = array_sum($tab3);
+// echo "La somme totale est : " . $total;
---
+++ PHP/Ex-6/index.php
@@ -0,0 +1,192 @@
+<?php
+
+
+// 1°) Soit le tableau associatif suivant : $chomage = array(‘Autriche' =>4.9, 'Allemagne' =>9.3 ,
+// 'Danemark' =>4.8 , ' Espagne' =>9.4 , 'France' =>9.7); 
+// 1. A l'aide de la boucle foreach afficher tous les pays et le taux de chômage correspondant
+
+
+// $chomage = array ("Autriche" =>4.9, 
+//                 "Allemagne" =>9.3 ,
+//                 "Danemark" =>4.8 , 
+//                 "Espagne" =>9.4 , 
+//                 "France" =>9.7
+// );
+
+// echo "J'affiche tout les pays et le taux de chomage correspondant: " , "\n";
+
+// foreach ($chomage as $key => $value) {
+//    echo $key , "  ", $value , " , ";
+// }
+
+// 2. A l'aide d'une boucle, écrire les instructions en PHP permettant de parcourir le tableau et
+// d’afficher le nom des pays européens ayant moins de 5 % de chômage.
+
+// $chomage = array ("Autriche" =>4.9, 
+//                 "Allemagne" =>9.3 ,
+//                 "Danemark" =>4.8 , 
+//                 "Espagne" =>9.4 , 
+//                 "France" =>9.7
+// );
+
+
+// $tauxChomage = 100 * 0.05;
+
+// foreach ($chomage as $key => $value) {
+//     if($value < $tauxChomage){
+//         echo $key , "  ", $value , " , ";
+//     }
+// }
+
+// 3. Afficher le nom du pays ayant le taux de chômage le plus faible
+
+// $chomage = array ("Autriche" =>4.9, 
+//                 "Allemagne" =>9.3 ,
+//                 "Danemark" =>4.8 , 
+//                 "Espagne" =>9.4 , 
+//                 "France" =>9.7
+// );
+
+
+
+// $tauxChomageMin = min($chomage);
+// $paysResultat = array_search($tauxChomageMin, $chomage);
+// echo "le pays avec le taux de chomage le plus faible est : " , $paysResultat , "avec un taux de : " , $tauxChomageMin;
+
+// 2°) Compléter le tableau suivant avec quelques noms et quelques notes : $tabNotes = array
+// (['boucher'] =>16, ['bourdette'] =>13 .........à compléter
+// 1. A l'aide d'une boucle afficher le nom de chaque élève et la note correspondante
+
+
+// $tabNotes = array ("boucher" => 16,
+//                    "chasseur" => 13,
+//                    "paysan" => 3,
+//                    "boulanger" => 8,
+//                    "pecheur" => 18
+// );
+
+// foreach ($tabNotes as $key => $value) {
+//     echo $key, " ", $value, "\n";
+// };
+
+// 2. Afficher la moyenne des notes
+
+// $tabNotes = array ("boucher" => 16,
+//                    "chasseur" => 13,
+//                    "paysan" => 3,
+//                    "boulanger" => 8,
+//                    "pecheur" => 18
+// );
+
+
+// foreach ($tabNotes as $key => $value) {
+//     echo $key, " ", $value, "\n";
+// };
+
+// $sum = array_sum($tabNotes);
+// $moyenne = $sum/count($tabNotes);
+
+// echo "la moyenne des notes est : ", " " , $moyenne;
+
+
+// 3°) $tabNotes = array(['prenot'] => array (2,10,14), ['perthuis'] => array (10,18,12) …à compléter avec
+// des élèves et des notes de votre choix
+// 1. Afficher le nom et les notes de chaque élève
+
+// $tabNotes = array('chasseur' => array (2,10,14),
+//                   'boucher' => array (10,18,12),
+//                   'paysan' => array (5,15,11),
+//                   'boulanger' => array (19,18,2),
+//                   'pecheur' => array (4,18,1)
+// );
+
+// foreach ($tabNotes as $nom => $notes) {
+//     echo $nom , "\n";
+//     foreach ($notes as $key => $value) {
+//         echo $value, " \n";
+//     }
+// };
+
+// 2. Afficher le nom et la moyenne de chaque élève
+
+// $tabNotes = array('chasseur' => array (2,10,14),
+//                   'boucher' => array (10,18,12),
+//                   'paysan' => array (5,15,11),
+//                   'boulanger' => array (19,18,2),
+//                   'pecheur' => array (4,18,1)
+// ); 
+
+
+// foreach ($tabNotes as $nom => $notes) {
+//         $sum = array_sum($notes);
+//         $moyenne = $sum/count($notes);
+
+//         echo " la moyenne des notes de " , $nom , " " , "est de " , $moyenne, "\n";
+
+// };
+
+// 3. Afficher les notes et la moyenne d'un élève dont le nom sera saisi au clavier (attention vous
+// devez vérifier que cet élève est bien présent dans le tableau)
+
+// $tabNotes = array(
+//     'chasseur' => array(2, 10, 14),
+//     'boucher' => array(10, 18, 12),
+//     'paysan' => array(5, 15, 11),
+//     'boulanger' => array(19, 18, 2),
+//     'pecheur' => array(4, 18, 1)
+// );
+
+// $saisie = readline("Écrit le nom de l'élève : ");
+
+// if (isset($tabNotes[$saisie])) {
+//     $notes = $tabNotes[$saisie];
+//     $sum = array_sum($notes);
+//     $moyenne = $sum / count($notes);
+
+//     echo "La moyenne des notes de $saisie est de $moyenne\n";
+//     echo "Les notes : ";
+//     foreach ($notes as $value) {
+//         echo $value . " ";
+//     }
+//     echo "\n";
+// } else {
+//     echo "Élève non trouvé.\n";
+//
+
+// 4. Faire un menu pour afficher les questions 1,2,3
+
+
+
+// $saisie = readline("Quel exercise souhaitez vous afficher ? Les exercises disponibles sont : 1 - 2 - 3 : ");
+
+
+// Exercices 8
+// 1°) Soit un tableau T à deux dimensions (12, 8) préalablement rempli de valeurs numériques.
+// Écrire un algorithme qui recherche la plus grande valeur au sein de ce tableau
+
+$T = [
+    [3, 1, 4, 1, 5, 9, 2, 6],
+    [5, 3, 5, 8, 9, 7, 9, 3],
+    [2, 3, 8, 4, 6, 2, 6, 4],
+    [3, 3, 8, 3, 2, 7, 9, 5],
+    [1, 0, 2, 5, 3, 5, 8, 9],
+    [7, 9, 3, 2, 300, 8, 4, 6],
+    [2, 7, 1, 0, 2, 8, 4, 1],
+    [4, 2, 9, 1, 6, 5, 3, 5],
+    [8, 9, 7, 9, 3, 2, 3, 8],
+    [4, 6, 2, 6, 4, 7, 3, 1],
+    [5, 9, 2, 7, 1, 4, 6, 5],
+    [9, 7, 9, 3, 2, 1, 0, 8]
+];
+
+$valeurMax = 0;
+
+for ($i=0; $i < 12 ; $i++) { 
+    for ($j= 0; $j < 8 ; $j++) { 
+        if ($T[$i][$j] > $valeurMax) {
+            $valeurMax = $T[$i][$j];
+        }
+    }
+}
+
+echo " la plus grande valeur de ce tableau multidimentionnel est : " , $valeurMax;
